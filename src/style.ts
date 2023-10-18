4ef46a2cb629fa04f97faabe83047c859ae7d31f
AlvaroDeCarvalho <alvarodecarvalho20@gmail.com>
2023-10-17 22:10:33 -0300

    realocando prettier para eslintrc.json

---
+++ src/style.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f0f0f0;
+    font-family: Arial, sans-serif;
+  }
+
+`
+export default GlobalStyle
